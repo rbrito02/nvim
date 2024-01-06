f9b153d7b275623440a182c8be43aa3767cbded2
Raudy Brito <raudyb02@gmail.com>
2024-01-06 16:59:20 -0500

    Reconfigured Harpoon Cmds

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,7 +2,9 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {}, { desc = "file search" })
 vim.keymap.set("n", "<C-p>", builtin.git_files, {}, { desc = "git file search" })
 vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "git branch manager" })
-vim.keymap.set("n", "<leader>ps", builtin.grep_string, { desc = "grep search" })
+vim.keymap.set("n", "<leader>ps", function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end, { desc = "grep search" })
 
 local colors = require("catppuccin.palettes").get_palette()
 local TelescopeColor = {
--- lua/tutturu/lazy.lua
+++ lua/tutturu/lazy.lua
@@ -54,6 +54,18 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>hn", function()
 				harpoon:list():next()
 			end)
+			vim.keymap.set("n", "<leader>hh", function()
+				harpoon:list():select(1)
+			end)
+			vim.keymap.set("n", "<leader>hj", function()
+				harpoon:list():select(2)
+			end)
+			vim.keymap.set("n", "<leader>hk", function()
+				harpoon:list():select(3)
+			end)
+			vim.keymap.set("n", "<leader>hl", function()
+				harpoon:list():select(4)
+			end)
 		end,
 	},
 
